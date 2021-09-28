19a01d509967f8fd527f9088adf3d37bfbe95dae
Pranoto Budi <pranotobudi.s@gmail.com>
2021-09-28 22:30:18 +0700

    add fs=empty to next.config

--- next.config.js
+++ next.config.js
@@ -5,5 +5,15 @@ module.exports = {
     env: {
         stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
         paypal_client_id: process.env.PAYPAL_CLIENT_ID,
+    },
+    webpack: (config, { isServer }) => {
+        // Fixes npm packages that depend on `fs` module
+        if (!isServer) {
+          config.node = {
+            fs: 'empty'
+          }
+        }
+    
+        return config
     }
 }
